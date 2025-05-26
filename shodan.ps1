a43df2196caa17379743751d41fcfef2bd816953
kaja <283833@student.pwr.edu.pl>
2025-05-26 16:06:23 +0200

    zmiany

---
+++ shodan.ps1
@@ -0,0 +1,12 @@
+﻿
+$ip  = Read-Host "podaj ip"
+$api = "q6zbAtFRZEMz5KEEP0LiVuA7C5Kuwkt3"
+
+
+
+Write-Host "podstawowe dane"
+$data = Invoke-WebRequest -Uri "https://api.shodan.io/shodan/host/$($ip)?key=$api" | ConvertFrom-Json
+$data | select-object "city", "ip", "last_update", "hostnames", "country_name", "domains", "org"
+
+Write-Host "otwarte porty"
+$data | select-object "ports"
